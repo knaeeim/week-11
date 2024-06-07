baf4c444057e95eccdd5322226b5865a5b45d29e
Khairul Bashar Naeeim <mdkhairulb01@gmail.com>
2024-06-07 22:32:17 +0530

    added day-3

---
+++ Day-3/B_378_QAQ_and_Mocha_s_Array.cpp
@@ -0,0 +1,76 @@
+#include<bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+
+template <typename T> using order_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define endl "\n"
+#define ll long long int
+#define pub push_back
+#define pob pop_back
+#define puf push_front
+#define pof pop_front
+#define vi vector<int>
+#define vll vector<ll>
+#define vp vector<pair<ll,ll>>
+#define sorta(arr) sort(arr.begin(), arr.end());
+#define sortd(arr) sort(arr.begin(), arr.end(), greater<int>());
+#define All(X) (X).begin(),(X).end()
+#define Unique(X) (X).erase(unique((X).begin(),(X).end()),(X).end())
+#define range(arr) for(auto el: arr) cout<<el<<" ";
+
+bool isPrime(int n){
+    if(n == 1) return false;
+    for(int i = 2; i*i <= n; i++){
+        if(n % i == 0){
+            return false;
+        }
+    }
+    return true;
+}
+
+
+int main()
+{
+    ios::sync_with_stdio(false); 
+    cin.tie(NULL); 
+    
+
+    int t; cin>>t; 
+
+    while(t--){
+        int n; cin>>n; 
+
+        vi v(n); 
+
+        for(int i = 0; i < n; i++) cin>>v[i]; 
+
+        sorta(v);
+        
+        vi final;
+
+        for(int i = 0; i < n; i++){
+            if(v[i] % v[0] != 0){
+                final.pub(v[i]);
+            }
+        }
+
+        bool found = true;
+
+        for(int i = 0; i < final.size(); i++){
+            if(final[i] % final[0] != 0){
+                found = false;
+                break;
+            }
+        } 
+
+        if(found) cout << "Yes" << endl; 
+        else cout << "No" << endl; 
+
+    }
+    return 0; 
+}
---
+++ Day-3/Sum_of_Modes.cpp
@@ -0,0 +1,56 @@
+#include<bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+
+template <typename T> using order_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define endl "\n"
+#define ll long long int
+#define pub push_back
+#define pob pop_back
+#define puf push_front
+#define pof pop_front
+#define vi vector<int>
+#define vll vector<ll>
+#define vp vector<pair<ll,ll>>
+#define sorta(arr) sort(arr.begin(), arr.end());
+#define sortd(arr) sort(arr.begin(), arr.end(), greater<int>());
+#define All(X) (X).begin(),(X).end()
+#define Unique(X) (X).erase(unique((X).begin(),(X).end()),(X).end())
+#define range(arr) for(auto el: arr) cout<<el<<" ";
+
+
+int main()
+{
+    ios::sync_with_stdio(false); 
+    cin.tie(NULL); 
+    
+
+    int t; cin>>t; 
+
+    while(t--){
+        int n; cin>>n; 
+        string s; cin>>s; 
+
+        int value  = 0; 
+        map<int, int> mp; 
+        mp[0]++;
+
+        ll ans = n *1LL * (n+1)/2;
+
+        for(auto e: s){
+            if(e == '1') value++;
+            else value--;
+
+            ans += mp[value]; 
+            mp[value]++;
+        }
+
+        cout << ans << endl;
+    }
+    return 0; 
+}
